0d8eead8fbd8b56c76b8b0b97eecf75ac0a507fa
James <verchjames@gmail.com>
2023-09-07 17:36:19 -0700

    Remove unused file, rename file to reflect class name

--- traverser/index.ts
+++ traverser/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import RobotVacuumCleaner from './robotVacuumCleanerTraverser';
+import RobotVacuumCleaner from './robotVacuumCleaner';
 
 dotenv.config();
 
